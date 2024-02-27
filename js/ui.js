1356a79f18cffdf8ad550f61e847884778938528
DESKTOP-U8PIS0N\computer <ase-hi@naver.com>
2024-02-27 14:28:55 +0900

    coding(ver.1.5)추가3 ------------------------------
    추천검색어 슬라이드추가
    employee-search-detail.html
    
    ui.js --> popup관련 함수 제거
    
    모든파일
    ui-pub.js 파일추가
    
    css 폴더 업데이트

--- js/ui.js
+++ js/ui.js
@@ -193,24 +193,9 @@
 				}
 		});			
   }
-
 		
 	
-	
-	// popup
-	function popOpen(popup){
-		var  $popup=$(popup);
-		$(window).scrollTop(0)
-		$('body').css('overflow', 'hidden');
-		$popup.show();
-	} 
-	
-	function popClose(popup){
-		var $popup = $(popup);
-		$('body').css('overflow', '');
-		$popup.hide();
-	} 
-	
+
 	$(function(){
 		layerMenu();
 		selectOpen();
